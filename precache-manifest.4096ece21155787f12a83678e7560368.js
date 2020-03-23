7273f6edeaa3b13a8fb96594e698b2d1559965b5
Oskar Gierszewski <gierszon10@gmail.com>
2020-03-24 00:34:02 +0100

    Updates

---
+++ precache-manifest.4096ece21155787f12a83678e7560368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c92e8711709c1173aa46e64c831f36",
+    "url": "/aliexpress-link-fixer/index.html"
+  },
+  {
+    "revision": "8d4d2c7e97c97d50cdda",
+    "url": "/aliexpress-link-fixer/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "193f4bb809c407ed5cf2",
+    "url": "/aliexpress-link-fixer/static/css/main.6349bedd.chunk.css"
+  },
+  {
+    "revision": "8d4d2c7e97c97d50cdda",
+    "url": "/aliexpress-link-fixer/static/js/2.d9940ff4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/aliexpress-link-fixer/static/js/2.d9940ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193f4bb809c407ed5cf2",
+    "url": "/aliexpress-link-fixer/static/js/main.b8cac3c3.chunk.js"
+  },
+  {
+    "revision": "f74abd2c53e0f7f1e9a3",
+    "url": "/aliexpress-link-fixer/static/js/runtime-main.ce7a8c65.js"
+  }
+]);
